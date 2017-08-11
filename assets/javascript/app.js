0c983e8f72364ff11a59c631aadcddc4ea968424
Fox <michaelscottfox1@gmail.com>
2017-08-11 16:15:38 -0700

    implemented boolean attributes for buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,52 @@
-//create an object variable of questions
-//create a timer function to countdown 10 seconds
+//create an object variable for questions
+//create a timer function to countdown 20 seconds
+//create a timer function for 5 seconds before moving to next questions
+//
 //timerUp function to check correct or incorrect answer
 //variable for right and wrong answers
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var questions = {
 
-  q1: {
+var q1 = {
     question: "What type of bread did Frank Costanza take back from George's girlfriend?",
     answers: ['Marble Rye', 'San Francisco Sourdough', 'Deli Bagels', 'Pumpernickel'],
     flags: [true, false, false, false],
     solution: 'A. Marble Rye'
-  },
+  };
 
-  q2: {
+var q2 = {
     question: "What kind of seafood did Kramer prepare at the Hamptons?",
     answers: ['Crab Legs', 'Lobster Tails', 'Grilled Tuna', 'Mahi Mahi'],
     flags: [false, true, false, false],
     solution: 'B. Lobster Tails'
-  }
-
-};
+  };
 
 $(document).ready(function(){
 
+  function makeButtons(q){
+    var btnIds=['A', 'B', 'C', 'D'];
+
+    for(var i =0; i<btnIds.length;i++){
+      var answerButton = $('<button>');
+      answerButton.text(btnIds[i]);
+      answerButton.addClass('btn btn-info');
+      answerButton.attr('data', q.flags[i]);
+      $('#answerButtons').append(answerButton);
+    }
+  }
+
+  function displayAnswer(q){
+    var correct = "The answer was " + q.solution;
+    $('#answerButtons').empty();
+    $('#questions').empty();
+    $('#answers').empty();
+    $('#answers').text(correct);
+
+  }
+
   function startTimer(){
-    var countdown = 20;
+    var countdown = 5;
     var intervalId;
 
     function run() {
@@ -39,6 +59,7 @@ $(document).ready(function(){
 
       if(countdown === 0){
         stop();
+        displayAnswer(q1);
       }
     }
 
@@ -48,15 +69,38 @@ $(document).ready(function(){
     run();
   }
 
+  function displaySlide(q){
+    $('#questions').text(q.question);
+    $('#answers').html("<br>A. "+q.answers[0]+"<br>B. "+q.answers[1]+"<br>C. "+q.answers[2]+"<br>D. " + q.answers[3]);
+
+  }
+
   function startGame(){
     startTimer();
-    $('#questions').text(questions.q1.question);
-    $('#answers').html("<br>"+questions.q1.answers[0]+"<br>"+questions.q1.answers[1]+"<br>"+questions.q1.answers[2]+"<br>"+questions.q1.answers[3]);
+    displaySlide(q1);
+    makeButtons(q1);
+
+    $('button').on("click", function() {
+      var answer = $(this).attr('data');
+      console.log(answer);
+
+      if(answer === true){
+        correctAnswers++;
+        console.log("Correct!");
+
+      }
+      else{
+        incorrectAnswers++;
+        console.log("Incorrect!");
+
+      }
+    })
   }
 
 
   $('#gameStart').on("click",function(){
     $('button').remove();
     startGame();
+
   });
 });
